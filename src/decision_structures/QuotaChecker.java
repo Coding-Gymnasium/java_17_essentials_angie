522ba800638a3794aa9e432b1ea5a43589f23d16
Nico Rithner <nicorithner@gmail.com>
2023-04-02 13:51:12 -0600

    Complete if-else statement exercise"

---
+++ src/decision_structures/QuotaChecker.java
@@ -0,0 +1,22 @@
+package decision_structures;
+
+import java.util.Scanner;
+
+public class QuotaChecker {
+    public static void main(String[] args) {
+        int quota = 10;
+
+        System.out.println("How many sales did you make this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        if(sales >= quota) {
+            System.out.println("Congrats! You've met your quota.");
+        }
+        else {
+            int salesShort = quota - sales;
+            System.out.println("Darn! You didn't meet your quota. You were " + salesShort + " sales short");
+        }
+    }
+}
